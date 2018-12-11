1f5ea1c2db97cf02d5c87184cfaa30b254107925
Theranjali Nilaweera <Theranjali.Nilaweera@spatialvision.com.au>
2018-12-11 19:36:44 +1100

    [3]  Refactor error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import cors from 'cors';
 import express from 'express';
 import nocache from 'nocache';
 
-import { wrap } from 'async-middleware';
 import { createLog } from './logs/logging';
 import { routes } from './routes';
 
@@ -16,10 +15,6 @@ app.use(nocache());
 app.use(cors({allowedHeaders: ['Content-Type', 'Authorization', 'Content-Encoding']}));
 app.use(bodyParser.json());
 app.use('/', routes);
-app.use(wrap( (req: express.Request, res: express.Response) => {
-    log.error('Error occured %j  and re %j ', req, res);
-    return Promise.reject(new Error('Error !!!!'));
-}));
 
 log.info('Start up');
 
