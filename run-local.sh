8d9f86d4f9628d4603ea18e829a6efa9b45c2181
Theranjali Nilaweera <Theranjali.Nilaweera@spatialvision.com.au>
2018-12-12 14:00:41 +1100

    Update command to run server from scratch

--- run-local.sh
+++ run-local.sh
@@ -2,7 +2,5 @@ echo "Change nvm"
 source ~/.bash_profile
 nvm use || { echo "Error changing node version"; exit 1; }
 
-echo "Install dependencies"
-npm i
 echo "Starting up local weather01 API"
-npm run serve || { echo "Error running local weather01 API"; exit 1; }
+npm run start-serve || { echo "Error running local weather01 API"; exit 1; }
